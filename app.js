3630e9009ef510bae71944fa5784663022f18ad7
Mitch Lillie <mitch@mitchlillie.com>
2015-10-07 20:04:52 -0700

    fix port confusion

--- app.js
+++ app.js
@@ -36,6 +36,4 @@ app.get('/casestudies/:title?', function (req, res) {
   }
 })
 
-app.listen(3000, function () {
-  console.log('The frontend server is running on port 3000!')
-})
+app.listen(process.env.PORT || 3000)
