6f92b02f161bac04dfcae8eb068fed43c0e74f0c
Mitch Lillie <mitch@mitchlillie.com>
2015-10-08 11:31:21 -0700

    Add testing to sidebar and storybad

--- public/js/main.js
+++ public/js/main.js
@@ -2,30 +2,6 @@
 /* (CDNs included in index.html) */
 
 $(document).ready(function () {
-  $('#storybar').affix({
-    offset: {
-      top: function () {
-        return (this.top = heightOf('.jumbotron', '#top', '#divider') - heightOf('.navbar-fixed-top'))
-      }
-         // bottom: function () {
-         //    return (this.bottom = this.top + heightOf('#bottom'))
-         // }
-         // #storybar(data-spy="affix" data-offset-top="2000")
-    }
-  })
-
-  $('#sidebar').affix({
-    offset: {
-      top: function () {
-        return (this.top = heightOf('.jumbotron') - heightOf('.navbar-fixed-top'))
-      },
-      bottom: function () {
-        return (this.bottom = heightOf('#footer', '#bottom', '#divider', '#sidebar') + 150) // this.top + heightOf('#top') + heightOf('#sidebar'))
-      }
-      // #sidebar(data-spy="affix" data-offset-top="1000" data-offset-bottom="700")
-    }
-  })
-
   function heightOf (el /*, el2, ... */) {
     var base = $(el).outerHeight(true)
     for (var z = 1; z < arguments.length; z++) {
@@ -35,4 +11,30 @@ $(document).ready(function () {
     console.log('base---->', base)
     return base
   }
+
+  $('#sidebar').affix({
+    offset: {
+      top: function () {
+        console.log('#sidebar this --->', this)
+        return (this.top = heightOf('.jumbotron') - heightOf('.navbar-fixed-top'))
+      },
+      bottom: function () {
+        return (this.bottom = heightOf('#footer', '#bottom', '#divider', '#sidebar') + 150) // this.top + heightOf('#top') + heightOf('#sidebar'))
+      }
+      // #sidebar(data-spy="affix" data-offset-top="1000" data-offset-bottom="700")
+    }
+  })
+
+  $('#storybar').affix({
+    offset: {
+      top: function () {
+        console.log('#storybar this --->', this)
+        return (this.top = heightOf('.jumbotron', '#top', '#divider') - heightOf('.navbar-fixed-top'))
+      }
+         // bottom: function () {
+         //    return (this.bottom = this.top + heightOf('#bottom'))
+         // }
+         // #storybar(data-spy="affix" data-offset-top="2000")
+    }
+  })
 })
