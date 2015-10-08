6b9ab5635e6f45633c7cbbf78b3cb57b5158923d
Mitch Lillie <mitch@mitchlillie.com>
2015-10-07 20:45:27 -0700

    testing for bootstrap affix

--- public/js/main.js
+++ public/js/main.js
@@ -1,37 +1,29 @@
 /* global $ */
 /* (CDNs included in index.html) */
 
-$(document).ready(function () {
-  console.log('bottom height...', heightOf('.jumbotron', '#top', '#divider') - heightOf('.navbar-fixed-top'))
-
-  reaffix()
-
-  $('#storybar').affix({
-    offset: {
-      top: function () {
-        return (this.top = heightOf('.jumbotron', '#top', '#divider') - heightOf('.navbar-fixed-top'))
-      }
-         // bottom: function () {
-         //    return (this.bottom = this.top + heightOf('#bottom'))
-         // }
-         // #storybar(data-spy="affix" data-offset-top="2000")
+$('#storybar').affix({
+  offset: {
+    top: function () {
+      return (this.top = heightOf('.jumbotron', '#top', '#divider') - heightOf('.navbar-fixed-top'))
     }
-  })
+       // bottom: function () {
+       //    return (this.bottom = this.top + heightOf('#bottom'))
+       // }
+       // #storybar(data-spy="affix" data-offset-top="2000")
+  }
 })
 
-function reaffix () {
-  $('#sidebar').affix({
-    offset: {
-      top: function () {
-        return (this.top = heightOf('.jumbotron') - heightOf('.navbar-fixed-top'))
-      },
-      bottom: function () {
-        return (this.bottom = this.top + heightOf('#top'))
-      }
-      // #sidebar(data-spy="affix" data-offset-top="1000" data-offset-bottom="700")
+$('#sidebar').affix({
+  offset: {
+    top: function () {
+      return (this.top = heightOf('.jumbotron') - heightOf('.navbar-fixed-top'))
+    },
+    bottom: function () {
+      return (this.bottom = heightOf('#top'))
     }
-  })
-}
+    // #sidebar(data-spy="affix" data-offset-top="1000" data-offset-bottom="700")
+  }
+})
 
 function heightOf (el /*, el2, ... */) {
   var base = $(el).outerHeight(true)
