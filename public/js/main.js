581c0364b9f43e96c3a2adfb76a51a6bfd949024
Mitch Lillie <mitch@mitchlillie.com>
2015-10-08 11:23:15 -0700

    document is now ready

--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,38 @@
 /* global $ */
 /* (CDNs included in index.html) */
 
-$('#storybar').affix({
-  offset: {
-    top: function () {
-      return (this.top = heightOf('.jumbotron', '#top', '#divider') - heightOf('.navbar-fixed-top'))
+$(document).ready(function () {
+  $('#storybar').affix({
+    offset: {
+      top: function () {
+        return (this.top = heightOf('.jumbotron', '#top', '#divider') - heightOf('.navbar-fixed-top'))
+      }
+         // bottom: function () {
+         //    return (this.bottom = this.top + heightOf('#bottom'))
+         // }
+         // #storybar(data-spy="affix" data-offset-top="2000")
     }
-       // bottom: function () {
-       //    return (this.bottom = this.top + heightOf('#bottom'))
-       // }
-       // #storybar(data-spy="affix" data-offset-top="2000")
+  })
+
+  $('#sidebar').affix({
+    offset: {
+      top: function () {
+        return (this.top = heightOf('.jumbotron') - heightOf('.navbar-fixed-top'))
+      },
+      bottom: function () {
+        return (this.bottom = heightOf('#footer', '#bottom', '#divider', '#sidebar') + 150) // this.top + heightOf('#top') + heightOf('#sidebar'))
+      }
+      // #sidebar(data-spy="affix" data-offset-top="1000" data-offset-bottom="700")
+    }
+  })
+
+  function heightOf (el /*, el2, ... */) {
+    var base = $(el).outerHeight(true)
+    for (var z = 1; z < arguments.length; z++) {
+      base += $(arguments[z]).outerHeight(true)
+    }
+    console.log('arg----->', arguments)
+    console.log('base---->', base)
+    return base
   }
 })
-
-$('#sidebar').affix({
-  offset: {
-    top: function () {
-      return (this.top = heightOf('.jumbotron') - heightOf('.navbar-fixed-top'))
-    },
-    bottom: function () {
-      return (this.bottom = heightOf('#footer', '#bottom', '#divider', '#sidebar') + 150) // this.top + heightOf('#top') + heightOf('#sidebar'))
-    }
-    // #sidebar(data-spy="affix" data-offset-top="1000" data-offset-bottom="700")
-  }
-})
-
-function heightOf (el /*, el2, ... */) {
-  var base = $(el).outerHeight(true)
-  for (var z = 1; z < arguments.length; z++) {
-    base += $(arguments[z]).outerHeight(true)
-  }
-  console.log('arg----->', arguments)
-  console.log('base---->', base)
-  return base
-}
